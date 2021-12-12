d53a8f305c0aca6e29ed2619d29b93052d6f51a4
Jrmyy <guiselin.j@gmail.com>
2021-12-12 06:45:41 +0100

    Day 12 2021

---
+++ src/main/kotlin/me/jeremy/aoc/y2021/Day12.kt
@@ -0,0 +1,62 @@
+package me.jeremy.aoc.y2021
+
+import me.jeremy.aoc.AOCUtils
+import me.jeremy.aoc.Day
+
+class Day12 : Day<List<Pair<String, String>>, Int> {
+    override fun runPartOne(): Int =
+        countPathsToEnd(getInput(), listOf("start"), 1)
+
+    override fun runPartTwo(): Int =
+        countPathsToEnd(getInput(), listOf("start"), 2)
+
+    override fun getInput(): List<Pair<String, String>> =
+        AOCUtils.getDayInput(2021, 12).map {
+            it.split("-").let { s -> Pair(s.first(), s.last()) }
+        }
+
+    private fun countPathsToEnd(
+        connexions: List<Pair<String, String>>,
+        beginning: List<String>,
+        maxSmallCavesVisit: Int
+    ): Int =
+        if (beginning.contains("end")) {
+            1
+        } else {
+            connexions.mapNotNull {
+                val otherPart = if (
+                    it.first == beginning.last()
+                ) it.second else if (
+                    it.second == beginning.last()
+                ) it.first else null
+                if (otherPart == "start") null else otherPart
+            }.sumOf {
+                val newBeginning = beginning + listOf(it)
+                val countPerSmallCaves = newBeginning.filter { s ->
+                    s.toLowerCase() == s && s != "end" && s != "start"
+                }.groupingBy { s -> s }.eachCount()
+                val maxOfSmallCaves = countPerSmallCaves.maxOfOrNull { e -> e.value }
+                val numOfMax = if (maxOfSmallCaves == null) {
+                    0
+                } else {
+                    countPerSmallCaves.count { e -> e.value == maxOfSmallCaves }
+                }
+                if (
+                    (maxOfSmallCaves != null && maxOfSmallCaves > maxSmallCavesVisit) ||
+                    (maxOfSmallCaves == maxSmallCavesVisit && maxSmallCavesVisit > 1 && numOfMax > 1)
+                ) {
+                    0
+                } else {
+                    countPathsToEnd(connexions, newBeginning, maxSmallCavesVisit)
+                }
+            }
+        }
+
+}
+
+
+fun main() {
+    val day = Day12()
+    println(day.runPartOne())
+    println(day.runPartTwo())
+}
