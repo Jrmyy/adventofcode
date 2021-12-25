373e83a5789c11c0ecadc01fe34c999791fedaa5
Jrmyy <guiselin.j@gmail.com>
2021-12-25 07:59:11 +0100

    2021 Day 24

---
+++ src/main/kotlin/me/jeremy/aoc/y2021/Day24.kt
@@ -0,0 +1,24 @@
+package me.jeremy.aoc.y2021
+
+import me.jeremy.aoc.AOCUtils
+import me.jeremy.aoc.Day
+
+class Day24: Day<List<String>, Long> {
+    override fun runPartOne(): Long {
+        // Done by hand
+        return 92967699949891
+    }
+
+    override fun runPartTwo(): Long {
+        // Done by hand
+        return 91411143612181
+    }
+
+    override fun getInput(): List<String> = AOCUtils.getDayInput(2021, 24)
+}
+
+fun main() {
+    val day = Day24()
+    println(day.runPartOne())
+    println(day.runPartTwo())
+}
