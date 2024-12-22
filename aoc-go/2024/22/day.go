b7014bbddc08283d938606280b3ec0dcd3e94da4
Jeremy Guiselin <jeremy.guiselin@jobteaser.com>
2024-12-22 06:52:44 +0100

    feat: 2024 day 22

---
+++ aoc-go/2024/22/day.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	"adventofcode-go/pkg/aocutils"
+)
+
+//go:embed input.txt
+var inputFile embed.FS
+
+func getPrice(i int) int {
+	s := strconv.Itoa(i)
+	price := aocutils.MustStringToInt(string(s[len(s)-1]))
+	return price
+}
+
+func getSecretNumber(secretNumber int) int {
+	multipliedSecretNumber := secretNumber * 64
+	secretNumber = (multipliedSecretNumber ^ secretNumber) % 16777216
+
+	dividedSecretNumber := int(math.Floor(float64(secretNumber) / 32.0))
+	secretNumber = (dividedSecretNumber ^ secretNumber) % 16777216
+
+	multipliedSecretNumber = secretNumber * 2048
+	secretNumber = (multipliedSecretNumber ^ secretNumber) % 16777216
+
+	return secretNumber
+}
+
+func runPartTwo(ipt []string) int {
+	var prices [][]int
+	var diffPrices [][]int
+	for _, line := range ipt {
+		secretNumber := aocutils.MustStringToInt(line)
+		unitaryPrices := []int{getPrice(secretNumber)}
+		var unitaryDiffPrices []int
+		for j := 0; j < 2000; j++ {
+			secretNumber = getSecretNumber(secretNumber)
+			unitaryPrices = append(unitaryPrices, getPrice(secretNumber))
+			unitaryDiffPrices = append(unitaryDiffPrices, unitaryPrices[len(unitaryPrices)-1]-unitaryPrices[len(unitaryPrices)-2])
+		}
+		prices = append(prices, unitaryPrices)
+		diffPrices = append(diffPrices, unitaryDiffPrices)
+	}
+
+	possibleSequences := map[string]int{}
+	for _, diffPrice := range diffPrices {
+		for idx := range diffPrice[:len(diffPrice)-3] {
+			repr := fmt.Sprintf("%d,%d,%d,%d", diffPrice[idx], diffPrice[idx+1], diffPrice[idx+2], diffPrice[idx+3])
+			possibleSequences[repr] -= 1
+		}
+	}
+
+	pq := aocutils.NewPriorityQueue[string]()
+	for seq, occ := range possibleSequences {
+		// The res is to be found among the sequence appearing a lot
+		pq.AddWithPriority(seq, occ)
+	}
+
+	res := 0
+	for pq.IsNotEmpty() {
+		seqStr := pq.ExtractMin()
+		rawSeq := strings.Split(seqStr, ",")
+		seq := make([]int, 4)
+		for idx := range rawSeq {
+			seq[idx] = aocutils.MustStringToInt(rawSeq[idx])
+		}
+		seqRes := 0
+		for buyerIdx, diffPrice := range diffPrices {
+			for idx := range diffPrice[:len(diffPrice)-4] {
+				eq := true
+				for k := range seq {
+					if seq[k] != diffPrice[idx+k] {
+						eq = false
+						break
+					}
+				}
+				if eq {
+					seqRes += prices[buyerIdx][idx+4]
+					break
+				}
+			}
+		}
+		res = max(res, seqRes)
+		// fmt.Println(res) to quickly give the solution
+	}
+
+	return res
+}
+
+func runPartOne(ipt []string) int {
+	res := 0
+	for _, line := range ipt {
+		secretNumber := aocutils.MustStringToInt(line)
+		for j := 0; j < 2000; j++ {
+			secretNumber = getSecretNumber(secretNumber)
+		}
+		res += secretNumber
+	}
+	return res
+}
+
+func main() {
+	lines := aocutils.MustGetDayInput(inputFile)
+	fmt.Println(runPartOne(lines))
+	fmt.Println(runPartTwo(lines))
+}
